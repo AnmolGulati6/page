7818a45b89e6792c1a6cb1509eeac13fb2c46289
Anmol Gulati <61374198+AnmolGulati6@users.noreply.github.com>
2026-03-10 08:47:01 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-50'
     >
       <div>
-        <img src={Logo} alt='Logo Image' style={{ width: '80px' }} />
+        <img src={Logo} alt='Logo' style={{ width: '80px' }} />
       </div>
 
       {/* menu */}
